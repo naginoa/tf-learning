b692f409a1ecb73edff9c917abc0ef3ee45f43e5
bigboy <18369185387@163.com>
2018-07-12 19:23:05 +0800

    其他

---
+++ kaggle常用/others.py
@@ -0,0 +1,229 @@
+﻿# 其他
+# 去除共线性
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import Imputer
+
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+class ReduceVIF(BaseEstimator, TransformerMixin):
+    def __init__(self, thresh=5.0, impute=True, impute_strategy='median'):
+        # From looking at documentation, values between 5 and 10 are "okay".
+        # Above 10 is too high and so should be removed.
+        self.thresh = thresh
+
+        # The statsmodel function will fail with NaN values, as such we have to impute them.
+        # By default we impute using the median value.
+        # This imputation could be taken out and added as part of an sklearn Pipeline.
+        if impute:
+            self.imputer = Imputer(strategy=impute_strategy)
+
+    def fit(self, X, y=None):
+        print('ReduceVIF fit')
+        if hasattr(self, 'imputer'):
+            self.imputer.fit(X)
+        return self
+
+    def transform(self, X, y=None):
+        print('ReduceVIF transform')
+        columns = X.columns.tolist()
+        if hasattr(self, 'imputer'):
+            X = pd.DataFrame(self.imputer.transform(X), columns=columns)
+        return ReduceVIF.calculate_vif(X, self.thresh)
+
+    @staticmethod
+    def calculate_vif(X, thresh=5.0):
+        # Taken from https://stats.stackexchange.com/a/253620/53565 and modified
+        dropped=True
+        while dropped:
+            # Loop repeatedly until we find that all columns within our dataset
+            # have a VIF value we're happy with.
+            variables = X.columns
+            dropped=False
+            vif = []
+            new_vif = 0
+            for var in X.columns:
+                new_vif = variance_inflation_factor(X[variables].values, X.columns.get_loc(var))
+                vif.append(new_vif)
+                if np.isinf(new_vif):
+                    break
+            max_vif = max(vif)
+            if max_vif > thresh:
+                maxloc = vif.index(max_vif)
+                #print 'Dropping {X.columns[{0}]} with vif={{1}}'.format(maxloc, max_vif)
+                print X.columns[maxloc]
+                print max_vif
+                X = X.drop([X.columns.tolist()[maxloc]], axis=1)
+                dropped=True
+        return X
+transformer = ReduceVIF()
+X = data_all
+# Only use 10 columns for speed in this example
+data_all = transformer.fit_transform(data_train[data_train.columns[0:50]], train_y)
+
+data_all.head()
+#  2. Stacking
+# Stacking Starter based on Allstate Faron's Script
+#https://www.kaggle.com/mmueller/allstate-claims-severity/stacking-starter/run/390867
+# Preprocessing from Alexandru Papiu
+#https://www.kaggle.com/apapiu/house-prices-advanced-regression-techniques/regularized-linear-models
+
+SEED = 1
+NFOLDS = 3
+import pandas as pd
+import numpy as np
+from scipy.stats import skew
+import xgboost as xgb
+from sklearn.cross_validation import KFold
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, Lasso
+from math import sqrt
+
+ntrain = data_train.shape[0]
+ntest = data_test.shape[0]
+print ntrain
+print ntest
+
+x_train = np.array(data_train)
+x_test = np.array(data_test)
+y_train = train_y
+kf = KFold(ntrain, n_folds=3, shuffle=True, random_state=SEED)
+class SklearnWrapper(object):
+    def __init__(self, clf, seed=0, params=None):
+        params['random_state'] = seed
+        self.clf = clf(**params)
+
+    def train(self, x_train, y_train):
+        self.clf.fit(x_train, y_train)
+
+    def predict(self, x):
+        return self.clf.predict(x)
+
+class XgbWrapper(object):
+    def __init__(self, seed=0, params=None):
+        self.param = params
+        self.param['seed'] = seed
+        self.nrounds = params.pop('nrounds', 250)
+
+    def train(self, x_train, y_train):
+        dtrain = xgb.DMatrix(x_train, label=y_train)
+        self.gbdt = xgb.train(self.param, dtrain, self.nrounds)
+
+    def predict(self, x):
+        return self.gbdt.predict(xgb.DMatrix(x))
+
+def get_oof(clf):
+    oof_train = np.zeros((ntrain,))
+    oof_test = np.zeros((ntest,))
+    oof_test_skf = np.empty((NFOLDS, ntest))
+
+    for i, (train_index, test_index) in enumerate(kf):
+        x_tr = x_train[train_index]
+        y_tr = y_train[train_index]
+        x_te = x_train[test_index]
+
+        clf.train(x_tr, y_tr)
+
+        oof_train[test_index] = clf.predict(x_te)
+        oof_test_skf[i, :] = clf.predict(x_test)
+
+    oof_test[:] = oof_test_skf.mean(axis=0)
+    return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
+
+
+et_params = {
+    'n_jobs': 16,
+    'n_estimators': 100,
+    'max_features': 0.5,
+    'max_depth': 12,
+    'min_samples_leaf': 2,
+}
+
+rf_params = {
+    'n_jobs': 16,
+    'n_estimators': 100,
+    'max_features': 0.2,
+    'max_depth': 12,
+    'min_samples_leaf': 2,
+}
+
+xgb_params = {
+    'seed': 0,
+    'colsample_bytree': 0.7,
+    'silent': 1,
+    'subsample': 0.7,
+    'learning_rate': 0.075,
+    'objective': 'reg:linear',
+    'max_depth': 4,
+    'num_parallel_tree': 1,
+    'min_child_weight': 1,
+    'eval_metric': 'rmse',
+    'nrounds': 500
+}
+
+
+
+rd_params={
+    'alpha': 10
+}
+
+
+ls_params={
+    'alpha': 0.005
+}
+
+
+
+xg = XgbWrapper(seed=SEED, params=xgb_params)
+et = SklearnWrapper(clf=ExtraTreesRegressor, seed=SEED, params=et_params)
+rf = SklearnWrapper(clf=RandomForestRegressor, seed=SEED, params=rf_params)
+rd = SklearnWrapper(clf=Ridge, seed=SEED, params=rd_params)
+ls = SklearnWrapper(clf=Lasso, seed=SEED, params=ls_params)
+
+
+
+xg_oof_train, xg_oof_test = get_oof(xg)
+et_oof_train, et_oof_test = get_oof(et)
+rf_oof_train, rf_oof_test = get_oof(rf)
+rd_oof_train, rd_oof_test = get_oof(rd)
+ls_oof_train, ls_oof_test = get_oof(ls)
+
+print("XG-CV: {}".format(sqrt(mean_squared_error(y_train, xg_oof_train))))
+print("ET-CV: {}".format(sqrt(mean_squared_error(y_train, et_oof_train))))
+print("RF-CV: {}".format(sqrt(mean_squared_error(y_train, rf_oof_train))))
+print("RD-CV: {}".format(sqrt(mean_squared_error(y_train, rd_oof_train))))
+print("LS-CV: {}".format(sqrt(mean_squared_error(y_train, ls_oof_train))))
+
+x_train = np.concatenate((xg_oof_train, et_oof_train, rf_oof_train, rd_oof_train, ls_oof_train), axis=1)
+x_test = np.concatenate((xg_oof_test, et_oof_test, rf_oof_test, rd_oof_test, ls_oof_test), axis=1)
+
+print("{},{}".format(x_train.shape, x_test.shape))
+
+
+dtrain = xgb.DMatrix(x_train, label=y_train)
+dtest = xgb.DMatrix(x_test)
+
+
+
+xgb_params = {
+    'seed': 0,
+    'colsample_bytree': 0.8,
+    'silent': 1,
+    'subsample': 0.6,
+    'learning_rate': 0.01,
+    'objective': 'reg:linear',
+    'max_depth': 1,
+    'num_parallel_tree': 1,
+    'min_child_weight': 1,
+    'eval_metric': 'rmse'}
+
+res = xgb.cv(xgb_params, dtrain, num_boost_round=1000, nfold=4, seed=SEED, stratified=False,
+             early_stopping_rounds=25, verbose_eval=10, show_stdv=True)
+
+best_nrounds = res.shape[0] - 1
+cv_mean = res.iloc[-1, 0]
+cv_std = res.iloc[-1, 1]
+
+print('Ensemble-CV: {0}+{1}'.format(cv_mean, cv_std))
